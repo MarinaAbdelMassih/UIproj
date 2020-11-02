8be9dd595b7c04385cb3d6a75f0cdfa663118df1
MarinaAbdelMassih <mabdelmassih@panarab-media.com>
2020-11-02 12:05:54 +0200

    init

---
+++ js/file.js
@@ -0,0 +1,9 @@
+
+$("#btnUp").click(function(){
+    $("html,body").animate({scrollTop:'0'},3000)
+    
+    });
+    $(function () {
+        $('[data-toggle="popover"]').popover()
+      })
+     
